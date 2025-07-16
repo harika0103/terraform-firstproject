d794cedbb4fa834ecd1fc9b2123f5c45f6b4070e
harika0103 <harikapati78@gmail.com>
2025-07-16 10:08:47 +0000

    storage

--- resource.tf
+++ resource.tf
@@ -13,6 +13,6 @@ resource "azurerm_storage_account" "saterra" {
  
 resource "azurerm_storage_container" "sacontainer" {
     name                  = "terra-container"
-    storage_account_name  = azurerm_storage_account.saterra.name
+    storage_account_id  = azurerm_storage_account.saterra.id
     container_access_type = "private"
 }
