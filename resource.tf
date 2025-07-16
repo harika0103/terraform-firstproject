b9a650bf4c43538866ffd747fe6f064d266e4f7b
harika0103 <harikapati78@gmail.com>
2025-07-16 09:54:03 +0000

    storage

--- resource.tf
+++ resource.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "storeterra" {
 }
  
 resource "azurerm_storage_account" "saterra" {
-    name                     = "terrastorage238"
+    name                     = "harika0103strg"
     resource_group_name      = azurerm_resource_group.storeterra.name
     location                 = azurerm_resource_group.storeterra.location
     account_tier             = "Standard"
