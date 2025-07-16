d32caae9b9bc6ba1742e7bd23027d448947fc81a
harika0103 <harikapati78@gmail.com>
2025-07-16 09:26:13 +0000

    storage

--- resource.tf
+++ resource.tf
@@ -1,4 +1,18 @@
-resource "azurerm_resource" "terra" {
- name = "terraform"
- location = "West US"   
+resource "azurerm_resource_group" "storeterra" {
+    name     = "example-resources"
+    location = "East US"
+}
+ 
+resource "azurerm_storage_account" "saterra" {
+    name                     = "terrastorage238"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "terra-container"
+    storage_account_name  = azurerm_storage_account.saterra.name
+    container_access_type = "private"
 }
